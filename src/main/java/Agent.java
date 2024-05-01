2da52fc0bd64becb266c532ded1f320e590c454d
Dewi-Payne <dewitbpayne@gmail.com>
2024-05-01 10:42:06 +0100

    added Agent class

---
+++ src/main/java/Agent.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Agent {
+	private int colour;
+
+	public Agent(int colour) {
+		this.setColour(colour);
+	}
+	
+	public int getMove(Game game) {
+		int rnd = new Random().nextInt(game.board.length);
+		if (game.isLegalMove(rnd, colour)){
+			return rnd;
+		}
+		return getMove(game);
+	}
+
+	public int getColour() {
+		return colour;
+	}
+
+	public void setColour(int colour) {
+		this.colour = colour;
+	}
+}
