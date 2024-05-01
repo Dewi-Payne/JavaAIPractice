6aefa850dd5194dd1152b3b47d170eb4c0eb4e03
Dewi-Payne <dewitbpayne@gmail.com>
2024-05-01 13:54:59 +0100

    added tests to TestGame class to test first two methods

--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -1,8 +1,38 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class TestGame {
 	@Test
-	public void testIsLegalMove(){
-		assert(true);
+	public void testIsLegalMove01(){
+		Game game = new Game();
+		assertTrue(game.isLegalMove(0, 1));
+	}
+	@Test
+	public void testIsLegalMove02(){
+		Game game = new Game();
+		game.move(0, 1);
+		game.move(1, -1);
+		assertFalse(game.isLegalMove(2, -1));
+		assertFalse(game.isLegalMove(1, 1));
+	}
+	@Test
+	public void testIsGameEnded01(){
+		Game game = new Game();
+		game.move(0, 1);
+		game.move(4, -1);
+		game.move(1, 1);
+		assertFalse(game.isGameEnded());
+	}
+	@Test
+	public void testIsGameEnded02(){
+		Game game = new Game();
+		game.move(0, 1);
+		game.move(4, -1);
+		game.move(1, 1);
+		game.move(7, -1);
+		game.move(2, 1);
+		assertTrue(game.isGameEnded());
 	}
 }
