bbdc1e21b9a8154c7b17e730957be626e997c82f
Dewi-Payne <dewitbpayne@gmail.com>
2024-05-01 10:47:46 +0100

    Fixed issue with board and turn variables

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,31 +5,31 @@ public class Game{
 
 	public boolean isLegalMove(int square, int player) {
 		if (isGameEnded()) {return false;}
-		if (player != turn) {return false;}
-		return board[square] != 0;
+		if (player != this.turn) {return false;}
+		return this.board[square] != 0;
 	}
 	public boolean isGameEnded() {
 		for (int i=0; i<=6; i+=3) {
-			if (board[i]==board[i+1] && board[i]==board[i+2] && board[i] != 0) {
+			if (this.board[i]==this.board[i+1] && this.board[i]==this.board[i+2] && this.board[i] != 0) {
 				return true;
 			}
 		}
 		for (int i=0; i<=3; i++) {
-			if (board[i]==board[i+3] && board[i]==board[i+6]&& board[i] != 0) {
+			if (this.board[i]==this.board[i+3] && this.board[i]==this.board[i+6]&& this.board[i] != 0) {
 				return true;
 			}
 		}
-		if (board[0]==board[4] && board[0] == board[8] && board[0] != 0) {
+		if (this.board[0]==this.board[4] && this.board[0] == this.board[8] && this.board[0] != 0) {
 			return true;
 		}
-		if (board[2]==board[4] && board[2] == board[6] && board[2] != 0) {
+		if (this.board[2]==this.board[4] && this.board[2] == this.board[6] && this.board[2] != 0) {
 			return true;
 		}
 		return false;
 	}
 	public void move(int square, int player) {
 		if (this.isLegalMove(square, player)) {
-			board[square] = player;
+			this.board[square] = player;
 		}
 	}
 }
